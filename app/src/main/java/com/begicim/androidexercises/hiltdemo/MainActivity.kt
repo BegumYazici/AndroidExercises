c3017604caf2211604137971910b5643f03105d9
Begum Yazici <yazicibegum@gmail.com>
2020-09-09 15:53:56 +0100

    Add hilt dependencies

--- app/src/main/java/com/begicim/androidexercises/hiltdemo/MainActivity.kt
+++ app/src/main/java/com/begicim/androidexercises/hiltdemo/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.begicim.androidexercises
+package com.begicim.androidexercises.hiltdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.begicim.androidexercises.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
