8c1a16f8929efc0f33fdbe8e5279f77c5c7ed066
Begum Yazici <yazicibegum@gmail.com>
2020-09-23 13:37:05 +0100

    Add application class with hilt annotation

---
+++ guesthewordapp/src/main/java/com/example/android/guesstheword/app/GuessTheWordApplication.kt
@@ -0,0 +1,7 @@
+package com.example.android.guesstheword.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GuessTheWordApplication : Application()
